9fc6e772e95b8886c17938bbbfc01511cef5d0da
shiavngi-jain-08 <myselfshivangi08@gmail.com>
2025-02-15 11:08:56 +0530

    merge

--- Backend/Routes/TherapyRoutes.js
+++ Backend/Routes/TherapyRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const {getAllTherapySessions,getTherapySessionById,createTherapySession,updateTherapyContext,updateTherapyChatHistory} = require("./../Controller/TherapyDataController")
+const {getAllTherapySessions,getTherapySessionById,createTherapySession,updateTherapyContextController,updateTherapyChatHistory} = require("./../Controller/TherapyDataController")
 const TherapyRoutes = express.Router();
 
 TherapyRoutes.get("/",getAllTherapySessions)
 TherapyRoutes.get("/:id",getTherapySessionById)
 TherapyRoutes.post("/",createTherapySession)
-TherapyRoutes.patch("/updateContext/:id",updateTherapyContext)
+TherapyRoutes.patch("/updateContext/:id",updateTherapyContextController)
 TherapyRoutes.patch("/updateChatHistory/:id",updateTherapyChatHistory)
 
 
